77f11f6e6a8bb892108b3bea75003f4529d34435
leejw <ni2309@naver.com>
2025-09-29 18:29:47 +0900

    fix: 에러 이름 변경

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 use crate::core::anchor::error::AnchorServiceError;
 
 #[derive(Debug, Error, PartialEq, Eq)]
-pub enum CryptoCoreError {
+pub enum CryptoServiceError {
     #[error("Anchor service error: {0}")]
     AnchorServiceError(#[from] AnchorServiceError),
 }
